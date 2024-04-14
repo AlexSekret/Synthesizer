ae5961c3d62f9fd25d410b91aab7ee8939fe0b91
Alexey <alexsekret3@gmail.com>
2024-04-14 18:13:49 +0300

    My first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiUnavailableException;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws MidiUnavailableException, InvalidMidiDataException, IOException, InterruptedException {
+            //РАССКОММЕНТИРОВАТЬ ТОЛЬКО ОДНУ СТРОКУ ЗА РАЗ
+//        SynthesizerReadTxtFile.read("D:\\Programming\\Java\\projects\\hexlet-homework\\Synthesizer\\src\\notes.txt");
+//        SynthesizerConsoleSequence.play();
+
+    }
+}
